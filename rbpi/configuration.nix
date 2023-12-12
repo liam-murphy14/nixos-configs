7890f789da8c9a78cb1f87d62ae21c66a78126be
liam-murphy14 <liam.murphy137@gmail.com>
2023-12-12 10:53:12 -0800

    open duc ports

--- rbpi/configuration.nix
+++ rbpi/configuration.nix
@@ -34,8 +34,8 @@
     networkmanager.enable = true;
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 22 ];
-      allowedUDPPorts = [ 5353 ];
+      allowedTCPPorts = [ 22 80 443 8245 ];
+      allowedUDPPorts = [ 5353 8245 ];
     };
   };
   services.openssh = {
