16958fcddd410b7a9939b9b464b9d77f7e2a38d0
liam-murphy14 <liam.murphy137@gmail.com>
2024-01-15 17:04:40 -0800

    add autocmd for docs

--- home_manager_configs/neovim/plugins/completions.lua
+++ home_manager_configs/neovim/plugins/completions.lua
@@ -56,3 +56,9 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
+  end,
+})
