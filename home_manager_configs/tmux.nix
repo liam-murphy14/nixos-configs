0ef27bd477ee3e05e3d69dbd798d1652ccd885c5
liam-murphy14 <liam.murphy137@gmail.com>
2024-01-20 18:36:41 -0800

    add better tmux colors (hopefully)

--- home_manager_configs/tmux.nix
+++ home_manager_configs/tmux.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs.tmux = {
     enable = true;
@@ -6,5 +6,14 @@
     customPaneNavigationAndResize = true;
     mouse = true;
     terminal = "screen-256color";
+    aggressiveResize = true;
+    historyLimit = 5000;
+    extraConfig = ''
+      status-style bg="#ABB2BF"
+      status-style fg="#282C34"
+      status-left-length 50
+      set-option -g pane-border-fg "#ABB2BF"
+      set-option -g pane-active-border-fg "#61AFEF"
+    '';
   };
 }
