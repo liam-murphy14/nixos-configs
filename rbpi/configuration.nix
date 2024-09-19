91273d01f774c9ffeee29b6784d89cb63082a48a
liam-murphy14 <liam.murphy137@gmail.com>
2024-09-18 21:59:36 -0700

    fix: remove sound option from rbpi

--- rbpi/configuration.nix
+++ rbpi/configuration.nix
@@ -29,7 +29,6 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
-  sound.enable = true;
   hardware.pulseaudio.enable = true;
 
   # NETWORK
